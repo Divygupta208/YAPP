88368a57ce65e10b04953468d1d2ad9b0154dd8b
Divy Gupta <divygupta208@gmail.com>
2024-11-13 15:52:20 +0530

    router client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import ChatWindow from "./components/chatwindow";
 import Signup from "./components/signup";
 import { Route, Router, Routes } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Signup />} />
-        <Route path="/chat" element={<ChatWindow />} />
+        <Route path="/chat" element={<Home />} />
       </Routes>
     </>
   );
